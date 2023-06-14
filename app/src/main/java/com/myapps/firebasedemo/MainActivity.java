83033815f840bf91d92d35e649121c1fd1a166b4
Praveen <pattarpraveen15@gmail.com>
2023-06-14 23:53:59 +0530

    first push

--- app/src/main/java/com/myapps/firebasedemo/MainActivity.java
+++ app/src/main/java/com/myapps/firebasedemo/MainActivity.java
@@ -1,22 +1,39 @@
 package com.myapps.firebasedemo;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnLogout;
     FirebaseAuth mAuth;
+    EditText edit;
+    Button add;
+    ListView listView;
 
-    @SuppressLint("MissingInflatedId")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +42,51 @@ public class MainActivity extends AppCompatActivity {
         btnLogout = findViewById(R.id.btnLogout);
         mAuth = FirebaseAuth.getInstance();
 
+        edit = findViewById(R.id.edit);
+        add = findViewById(R.id.add);
+        listView = findViewById(R.id.listView);
+
         btnLogout.setOnClickListener(v -> {
             mAuth.signOut();
             startActivity(new Intent(MainActivity.this, login.class));
+            finish();
         });
 
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text_name = edit.getText().toString();
+                if (TextUtils.isEmpty(text_name)){
+                    Toast.makeText(MainActivity.this, "no name entered", Toast.LENGTH_SHORT).show();
+                }else {
+                    FirebaseDatabase.getInstance().getReference().child("Languages").child("Name").setValue(text_name);
+
+                }
+            }
+        });
+
+        ArrayList<String> list = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.activity_list_item, list);
+        listView.setAdapter(adapter);
+
+        FirebaseDatabase.getInstance().getReference().child("Languages").child("n5").setValue("XML");
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Languages");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+                list.clear();
+                for (DataSnapshot snapshot : datasnapshot.getChildren()) {
+                    list.add(snapshot.getValue().toString());
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     protected void onStart(){
